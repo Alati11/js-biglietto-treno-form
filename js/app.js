4c7841653e7b11ac1cab7bd69f7d7652ffd2b16f
Alati11 <peppealati@gmail.com>
2023-10-05 23:35:17 +0200

    correction eventlistener

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ const inputDOMElement = document.getElementById('name');
 const kmDOMElement = document.getElementById('km');
 console.log(kmDOMElement);
 
-const km = parseFloat(inputDOMElement.value);
+let km = parseFloat(inputDOMElement.value);
 console.log(km);
 
 // prendo il pulsante dal DOM
@@ -20,22 +20,26 @@ const selDOMElement = document.getElementById('discount')
 let etaUser;
 
 //leggo clicca scelta
-btnDOMElement.addEventListener('click', function () {
+btnDOMElement.addEventListener('click', 
+function () {
     console.log("CLICK")
-    console.log('selDOMElement.value', selDOMElement.value)    
-
-// recupero dall'input il valore delláttributo value
-    const km = parseFloat(kmDOMElement.value)
-	console.log('km', km)
-})
+    console.log('selDOMElement.value', selDOMElement.value) 
     
-// - calcolare il prezzo totale del viaggio
+// recupero dall'input il valore dell'attributo value
+    let km = parseFloat(kmDOMElement.value)
+	console.log('km', km)
+
+    // - calcolare il prezzo totale del viaggio
 //     - calcolare prezzo base (0.21 € al km)
 // - moltiplicare il prezzo al km per i km da percorrere
 let price = (parseFloat(0.21));  
 let priceBase = km * price;    
-let priceFinal
-priceFinal = priceBase;
+let priceFinal = priceBase;
+console.log(priceFinal)
+
+if (selDOMElement == 1) {
+console.log(parseFloat(priceBase)); 
+}
 
 // - Se l'utente è minorenne         
 //  - sottrarre il 20% dal prezzo base
@@ -44,6 +48,9 @@ if (etaUser < 18) {
     let priceUnder = (priceBase - offUnder);
     priceFinal = priceUnder;
     console.log(parseFloat(priceUnder)); 
+    if (selDOMElement == 0) {
+    console.log(parseFloat(priceUnder)); 
+    }
 }
 
 // Se L'utente è over 65
@@ -53,7 +60,11 @@ if (etaUser > 65) {
     let priceOver = (priceBase - offOver);
     priceFinal = priceOver;
     console.log(parseFloat(priceOver));
+    if (selDOMElement == 2) {
+        console.log(parseFloat(priceOver)); 
+    }
 }
+})
 
 // - stampare il prezzo finale
 // arrotondare a 2 cifre decimali
@@ -61,4 +72,7 @@ priceFinal = priceFinal.toFixed(2)
 let messageDomElement = document.getElementById('price');
 console.dir(messageDomElement);
 
-//- Modificare l'innerHTML dello span con il prezzo finale del biglietto 
+    // creazione dati biglietto in pagina
+    document.getElementById("name").innerHTML = inputDOMElement;
+    // document.getElementById("discount").innerHTML = discountType;
+    document.getElementById("price").innerHTML = priceFinal + "€";
